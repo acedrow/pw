928540468c9b7738e434214c4ed0e539f1e57a95
Linden Holt <ace.holt@gmail.com>
2020-06-22 21:43:43 -0500

    starting fresh

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React, { Component} from "react";
 import {hot} from "react-hot-loader";
 import "./App.css";
-import { AppConsole } from './console/components/AppConsole';
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1 style={styleTest} id='helloWorld'> Hello, Worldd! </h1>
-        <AppConsole></AppConsole>
+        <h1> Hello, World! </h1>
       </div>
     );
   }
 }
 
-const styleTest = {
-  color: 'blue',
-}
-
-//export default App
 export default hot(module)(App);
