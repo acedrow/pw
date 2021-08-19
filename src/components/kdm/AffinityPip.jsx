5f7751d1925e00ec9ed824b74f7717bf85e42347
Linden Holt <ace.holt@gmail.com>
2021-08-19 18:07:48 -0500

    starting over

--- src/components/kdm/AffinityPip.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-export const AffinityPip = (props) => {
-  //TODO: finish styling using the pip position
-  return <div style={{ height: '10%', width: '10%' }}></div>
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,52 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
+  entry: path.resolve(__dirname, "src", "index.tsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  mode: "development",
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        options: { presets: ['@babel/env'] },
+        test: /\.[jt]sx?$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader'],
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
       },
     ],
   },
-  resolve: { extensions: ['*', '.js', '.jsx'] },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/dist',
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/index.html"),
+    }),
+    new CleanWebpackPlugin(),
+  ],
   devServer: {
-    historyApiFallback: true,
-    contentBase: path.join(__dirname, 'dist/'),
     port: 3000,
-    publicPath: 'http://localhost:3000/dist',
-    hotOnly: true,
+    hot: true,
+    compress: true,
+    open: true,
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
 }
