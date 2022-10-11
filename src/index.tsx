1863c2884f3d76d9a10f002a677acb7fc3bf7703
acedrow <ace.holt@gmail.com>
2022-10-11 13:38:39 -0500

    clean up routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,12 @@
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import ChessMain from './chess'
 import Homepage from './home/homepage'
-import KdmMainpage from './kdm'
-import Skunkworks from './skunkworks'
 import GlobalStyle from './styles/globalStyle'
 
 ReactDOM.render(
   <Router>
     <GlobalStyle />
-
     <Switch>
-      {/* <Route exact path="/kdm">
-        <KdmMainpage />
-      </Route> */}
-      {/* <Route exact path="/chess">
-        <ChessMain />
-      </Route> */}
-      <Route exact path="/sw">
-        <Skunkworks />
-      </Route>
       <Route exact path="/">
         <Homepage />
       </Route>
